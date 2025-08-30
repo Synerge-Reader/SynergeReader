c8cdd400fff69df09d9ba88545a560f625e110c0
Ondrey, Julian <jondrey2014@gmail.com>
2025-08-29 21:27:22 -0500

    test

--- synerge-reader-frontend/src/App.jsx
+++ synerge-reader-frontend/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
     }
 
     setIsLoading(true);
-    fetch("http://localhost:5000/ask", {
+    fetch(process.env.REACT_APP_BACKEND_URL + "/ask", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ 
@@ -55,7 +55,7 @@ function App() {
         setIsLoading(false);
         setAskOpen(false);
         // Refresh history
-        fetch("http://localhost:5000/history")
+        fetch(process.env.REACT_APP_BACKEND_URL + "/history")
           .then((res) => res.json())
           .then((data) => setHistory(data));
       })
--- synerge-reader-frontend/src/components/FileUpload.js
+++ synerge-reader-frontend/src/components/FileUpload.js
@@ -21,7 +21,7 @@ const FileUpload = ({ onFileParsed, setIsLoading, setError }) => {
       const blob = new Blob([textContent], { type: 'text/plain' });
       formData.append('file', blob, fileName);
       
-      const response = await fetch('http://localhost:5000/upload', {
+      const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/upload', {
         method: 'POST',
         body: formData
       });
